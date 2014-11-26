4bfd3ccc33618f9c8b7f627590fb68125ea5baa3
Brian Lau <brian.lau@upmc.fr>
2014-11-26 18:56:10 +0100

    minimal documentation + refs

--- +dep/dcorr.m
+++ +dep/dcorr.m
@@ -1,3 +1,43 @@
+% DCORR                       Distance correlation
+% 
+%     d = dcorr(x,y,modified)
+%
+%     INPUTS
+%     x
+%     y
+%
+%     OPTIONAL
+%     modified - boolean indicating bias-correction (default=false)
+%
+%     OUTPUTS
+%     d - distance covariance between x,y
+%
+%     REFERENCE
+%     Szekely et al (2007). Measuring and testing independence by correlation 
+%       of distances. Ann Statist 35: 2769-2794
+%     Szekely & Rizzo (2013). The distance correlation t-test of independence 
+%       in high dimension. J Multiv Analysis 117: 193-213
+%
+%     SEE ALSO
+%     dcorr, dcorrtest
+
+%     $ Copyright (C) 2014 Brian Lau http://www.subcortex.net/ $
+%     The full license and most recent version of the code can be found at:
+%     https://github.com/brian-lau/highdim
+%
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 function d = dcorr(x,y,modified)
 
 if nargin < 3
--- +dep/dcorrtest.m
+++ +dep/dcorrtest.m
@@ -1,3 +1,44 @@
+% DCORRTEST                   Distance correlation test of independence
+% 
+%     [pval,stat] = dcorrtest(x,y,method)
+%
+%     INPUTS
+%     x
+%     y
+%
+%     OPTIONAL
+%     method - 't' indicates high-dimensional t-test, otherwise bootstrap
+%
+%     OUTPUTS
+%     pval -
+%     stat - 
+%
+%     REFERENCE
+%     Szekely et al (2007). Measuring and testing independence by correlation 
+%       of distances. Ann Statist 35: 2769-2794
+%     Szekely & Rizzo (2013). The distance correlation t-test of independence 
+%       in high dimension. J Multiv Analysis 117: 193-213
+%
+%     SEE ALSO
+%     dcorr, dcorrtest
+
+%     $ Copyright (C) 2014 Brian Lau http://www.subcortex.net/ $
+%     The full license and most recent version of the code can be found at:
+%     https://github.com/brian-lau/highdim
+%
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 function [pval,stat] = dcorrtest(x,y,method)
 
 if nargin < 3
