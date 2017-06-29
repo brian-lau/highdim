0a87d8a33f4f7f7b48e29a4903cd9e45ed354b75
Brian Lau <brian.lau@upmc.fr>
2017-06-29 17:35:18 +0200

    ENH: Update permutation testing for DCORR

--- +dep/dcorrtest.m
+++ +dep/dcorrtest.m
@@ -1,6 +1,6 @@
 % DCORRTEST                   Distance correlation test of independence
 % 
-%     [pval,r,T] = dcorrtest(x,y,varargin)
+%     [pval,r,stat] = dcorrtest(x,y,varargin)
 %
 %     Given a sample X1,...,Xn from a p-dimensional multivariate distribution,
 %     and a sample Y1,...,Xn from a q-dimensional multivariate distribution,
@@ -8,13 +8,24 @@
 %
 %     H0 : X and Y are mutually independent
 %
+%     The default test is based on a modified distance correlation statistic 
+%     that when suitably transformed converges to a Student t distribution 
+%     under independence (Szekely & Rizzo 2013). The resulting t-test is 
+%     unbiased for sample sizes greater than three and all significance 
+%     levels. 
+%
+%     Several different permutation methods are also available. See DCOVTEST
+%     for details. These are included mostly for testing since the t-test
+%     is well-behaved even in small samples, and very computationally efficient.
+%
 %     INPUTS
 %     x - [n x p] n samples of dimensionality p
 %     y - [n x q] n samples of dimensionality q
 %
-%     OPTIONAL
+%     OPTIONAL (as name/value pairs, order irrelevant)
 %     method - 't'          - t-test from Szekely & Rizzo (2013), DEFAULT 
-%              'perm'       - randomization using permutation of the rows &
+%              'pearson'    - Pearson type III approx by moment matching
+%              'perm-dist'  - randomization using permutation of the rows &
 %                             columns of distance matrices
 %              'perm-brute' - brute force randomization, directly permuting
 %                             one of the inputs, which requires recalculating 
@@ -23,18 +34,20 @@
 %
 %     OUTPUTS
 %     pval - p-value
-%     r    - distance correlation, bias-corrected if method = 't' (default)
-%     T    - t-statistic
+%     r    - distance correlation, bias-corrected
+%     stat - test statistic
 %
 %     EXAMPLE
 %     rng(1234);
-%     p = 2000;
-%     n = 500;
-%     X = rand(n,p);  Y = X.^2 + 1*randn(n,p);
+%     p = 100;
+%     n = 2000;
+%     X = rand(n,p);  Y = X.^2 + 15*randn(n,p);
 %
-%     tic;[pval,r] = dep.dcorrtest(X,Y,'method','t'); toc
+%     tic;[pval,r] = dep.dcorrtest(X,Y); toc % default t-test
 %     [pval , r]
-%     tic;[pval,r] = dep.dcorrtest(X,Y,'method','perm','nboot',200);toc
+%     tic;[pval,r] = dep.dcorrtest(X,Y,'method','pearson'); toc
+%     [pval , r]
+%     tic;[pval,r] = dep.dcorrtest(X,Y,'method','perm-dist','nboot',200);toc
 %     [pval , r]
 %     tic;[pval,r] = dep.dcorrtest(X,Y,'method','perm-brute','nboot',200);toc
 %     [pval , r]
@@ -62,7 +75,7 @@
 %     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 %     GNU General Public License for more details.
 
-function [pval,r,T] = dcorrtest(x,y,varargin)
+function [pval,r,stat] = dcorrtest(x,y,varargin)
 
 par = inputParser;
 par.KeepUnmatched = true;
@@ -78,38 +91,69 @@ assert(n == size(y,1),'DCORRTEST requires x and y to have the same # of samples'
 nboot = par.Results.nboot;
 
 switch lower(par.Results.method)
-   case {'t','ttest','t-test'}
-      r = dep.dcorr(x,y,'unbiased',true);
-      v = n*(n-3)/2;
-      T = sqrt(v-1) * r/sqrt(1-r^2);
-      pval = 1 - tcdf(T,v-1);
-      %tcdf(T,v-1,'upper')
+   case {'pearson'}
+      if ~isfield(par.Unmatched,'unbiased')
+         % Override dcov default, we generally want unbiased dcorr
+         [d,dvx,dvy,A,B] = dep.dcov(x,y,'unbiased',true,par.Unmatched);
+      else
+         [d,dvx,dvy,A,B] = dep.dcov(x,y,par.Unmatched);
+      end
+      r = d/sqrt(dvx*dvy);
+      [mu,sigma2,skew] = utils.permMoments(A,B); % Exact moments
+      
+      if isfield(par.Unmatched,'unbiased') && par.Unmatched.unbiased
+         stat = (n*(n-3))*d; %  = sum(sum(A.*B)) for unbiased estimator
+      elseif ~isfield(par.Unmatched,'unbiased')
+         stat = (n*(n-3))*d; %  = sum(sum(A.*B)) for unbiased estimator
+      else
+         stat = (n^2)*d^2; %  = sum(sum(A.*B)) for biased estimator
+      end
+      stat = (stat - mu)/sqrt(sigma2);
+      if skew >= 0
+         pval = gamcdf(stat - (-2/skew),4/skew^2,skew/2,'upper');
+      else
+         as = abs(skew);
+         pval = gamcdf(skew/as*stat + 2/as,4/skew^2,as/2);         
+      end
       
       return;
-   case {'perm'}      
+   case {'t','ttest','t-test'}
+      if isfield(par.Unmatched,'unbiased') && ~par.Unmatched.unbiased
+         error('This method is only valid for UNBIASED estimator');
+      elseif ~isfield(par.Unmatched,'unbiased')
+         r = dep.dcorr(x,y,'unbiased',true,par.Unmatched);
+      else
+         r = dep.dcorr(x,y,par.Unmatched);
+      end
+      v = n*(n-3)/2;
+      stat = sqrt(v-1) * r/sqrt(1-r^2);
+      pval = tcdf(stat,v-1,'upper');
+      
+      return;
+   case {'perm-dist'}      
       a = sqrt(utils.sqdist(x,x));
       b = sqrt(utils.sqdist(y,y));
       [d,dvx,dvy] = dep.dcov(a,b,'dist',true,'unbiased',true);
       r = d/sqrt(dvx*dvy);
 
-      boot = zeros(nboot,1);
+      stat = zeros(nboot,1);
       for i = 1:nboot
          ind = randperm(n);
          [d2,dvx2,dvy2] = dep.dcov(a,b(ind,ind),'dist',true,'unbiased',true);
-         boot(i) = d2/sqrt(dvx2*dvy2);
+         stat(i) = d2/sqrt(dvx2*dvy2);
       end
    case {'perm-brute'}
       [d,dvx,dvy] = dep.dcov(x,y,'unbiased',true);
       r = d/sqrt(dvx*dvy);
 
-      boot = zeros(nboot,1);
+      stat = zeros(nboot,1);
       for i = 1:nboot
          ind = randperm(n);
          [d2,dvx2,dvy2] = dep.dcov(x,y(ind,:),'unbiased',true);
-         boot(i) = d2/sqrt(dvx2*dvy2);
+         stat(i) = d2/sqrt(dvx2*dvy2);
       end
    otherwise
       error('Unrecognized test method');
 end
 
-pval = (1 + sum(boot>r)) / (1 + nboot);
+pval = (1 + sum(stat>r)) / (1 + nboot);
