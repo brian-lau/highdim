49c7f48330a94678b49397868badc317831a1f22
Brian Lau <brian.lau@upmc.fr>
2017-07-02 18:17:31 +0200

    ENH: add "approx" parameter to utils.rbf to allow random feature map approximations

--- +utils/rbf.m
+++ +utils/rbf.m
@@ -1,9 +1,10 @@
 % RBF                         Kernel matrix using Gaussian radial basis
-% 
-%     k = rbf(sigma,x,y)
+%
+%     k = rbf(sigma,x,y,varargin)
 %
 %     INPUTS
-%     x - [m x p] m samples of dimensionality p
+%     sigma - scalar, standard deviation of Gaussian kernel
+%     x     - [m x p] m samples of dimensionality p
 %
 %     OPTIONAL
 %     y - [n x p] n samples of dimensionality p
@@ -11,7 +12,7 @@
 %     OUTPUTS
 %     k - kernel matrix
 
-%     $ Copyright (C) 2014 Brian Lau http://www.subcortex.net/ $
+%     $ Copyright (C) 2017 Brian Lau, brian.lau@upmc.fr $
 %     The full license and most recent version of the code can be found at:
 %     https://github.com/brian-lau/highdim
 %
@@ -19,22 +20,42 @@
 %     it under the terms of the GNU General Public License as published by
 %     the Free Software Foundation, either version 3 of the License, or
 %     (at your option) any later version.
-% 
+%
 %     This program is distributed in the hope that it will be useful,
 %     but WITHOUT ANY WARRANTY; without even the implied warranty of
 %     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 %     GNU General Public License for more details.
 
-function k = rbf(sigma,x,y)
+function k = rbf(sigma,x,y,varargin)
 
-xq = sum(x.*x,2);
-if nargin == 2
-   k = bsxfun(@plus,xq,xq') - 2*x*x';
-else
-   yq = sum(y.*y,2);
-   k = bsxfun(@plus,xq,yq') - 2*x*y';
+par = inputParser;
+par.KeepUnmatched = true;
+addRequired(par,'sigma',@isnumeric);
+addRequired(par,'x',@isnumeric);
+addRequired(par,'y',@isnumeric);
+addParamValue(par,'approx','none',@ischar);
+parse(par,sigma,x,y,varargin{:});
+
+switch lower(par.Results.approx)
+   case {'none'}
+      % % one-liner scales poorly
+      % k = exp(-pdist2(x,y).^2/2*sigma^2);
+      xq = sum(x.*x,2);
+      if isempty(y)
+         k = bsxfun(@plus,xq,xq') - 2*x*x';
+      else
+         yq = sum(y.*y,2);
+         k = bsxfun(@plus,xq,yq') - 2*x*y';
+      end
+      k = exp(-k/(2*sigma^2));
+   case {'random' 'rfm'}
+      phix = utils.rfm(sigma,x,par.Unmatched);
+      if isempty(y)
+         k = phix'*phix;
+      else
+         phiy = utils.rfm(sigma,y,par.Unmatched);
+         k = phix'*phiy;
+      end
+   otherwise
+      error('Unrecognized method');
 end
-k = exp(-k/(2*sigma^2));
-
-% % one-liner scales poorly
-% k = exp(-pdist2(x,y).^2/2*sigma^2);
