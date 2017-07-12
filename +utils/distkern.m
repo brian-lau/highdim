b78a5a8f3bc5d49e627a73b4e033d431202cc4dc
Brian Lau <brian.lau@upmc.fr>
2017-07-12 11:57:03 +0200

    ENH: distkern allows exponentiation parameter

--- +utils/distkern.m
+++ +utils/distkern.m
@@ -1,8 +1,23 @@
-function k = distkern(X,Y)
+% Sejdinovic et al, pg. 2272, example 15
+
+function k = distkern(X,Y,varargin)
+
+par = inputParser;
+par.KeepUnmatched = true;
+addRequired(par,'X',@isnumeric);
+addRequired(par,'Y',@isnumeric);
+addParamValue(par,'index',1,@(x) isscalar(x) && (x>0) && (x<=2));
+parse(par,X,Y,varargin{:});
 
 Yt = Y';
 XX = sqrt(sum(X.*X,2));
 YY = sqrt(sum(Yt.*Yt));
 D = sqrt(utils.sqdist(X,Y));
+
+if par.Results.index ~= 1
+   XX = XX.^par.Results.index;
+   YY = YY.^par.Results.index;
+   D = D.^par.Results.index;
+end
 
 k = 0.5 * (bsxfun(@plus,XX,YY) - D);
