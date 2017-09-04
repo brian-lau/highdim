0de4787579e6e7612a6b45b8775099866f584477
Brian Lau <brian.lau@upmc.fr>
2017-09-04 11:15:46 +0200

    ENH: Randomized SVD (rsvd) optional

--- +utils/nystrom.m
+++ +utils/nystrom.m
@@ -1,17 +1,16 @@
 % NYSTROM                     Nystrom approximation of kernel matrix
 % 
-%     [k,sigma] = rbf(x,y,varargin)
+%     [phi,K] = nystrom(X,varargin)
 % 
 %     INPUTS
 %     X     - [n x p] n samples of dimensionality p
 % 
 %     OPTIONAL
-%     k    - scalar, number of columns to sample
+%     c    - scalar, number of columns to sample (without replacement)
 %     rsvd - boolean indicating whether to use randomized SVD
 %     tol  - scalar tolerance for truncating small singular values
 % 
-%     Additional name/value pairs are passed through to function for 
-%     estimating the kernel when using an approximation method.
+%     Additional name/value pairs are passed through to RSVD if true.
 % 
 %     OUTPUTS
 %     phi   - approximate feature mapped data
@@ -29,25 +28,26 @@ function [phi,K] = nystrom(X,varargin)
 par = inputParser;
 par.KeepUnmatched = true;
 addRequired(par,'X',@isnumeric);
-addParamValue(par,'k',[],@(x) isnumeric(x) && isscalar(x));
+addParamValue(par,'c',[],@(x) isnumeric(x) && isscalar(x));
 addParamValue(par,'rsvd',false,@islogical);
 addParamValue(par,'tol',[],@(x) isnumeric(x) && isscalar(x));
 parse(par,X,varargin{:});
 
-n = size(X,1);
-if isempty(par.Results.k)
-   k = fix(0.5*n);
+[n,p] = size(X);
+if isempty(par.Results.c)
+   c = fix(0.25*n); % Default to 25% columns
 else
-   k = par.Results.k;
+   c = min(par.Results.c,n);
 end
 
 ind = randperm(n);
-ind = ind(1:k);
+ind = ind(1:c);
 C = utils.kernel(X,X(ind,:),par.Unmatched); % C = K(:,ind)
 W = C(ind,:);
 
 if par.Results.rsvd
-   [U,S] = rsvd(W,par.Unmatched);
+   %[U,S] = utils.rsvd(W,par.Unmatched);
+   [U,S] = utils.rsvd(W,30,10,3);
 else
    [U,S] = svd(W);
 end
@@ -57,9 +57,9 @@ if isempty(par.Results.tol)
 else
    tol = par.Results.tol;
 end
-k = sum(s > tol);
-s = 1./sqrt(s(1:k));
-UW = bsxfun(@times,U(:,1:k),s');
+c = sum(s > tol);
+s = 1./sqrt(s(1:c));
+UW = bsxfun(@times,U(:,1:c),s');
 
 phi = C*UW;
 
